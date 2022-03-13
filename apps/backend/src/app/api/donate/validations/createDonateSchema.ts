4a6cf9ef4f994446b3d808bac2b1206e4fc1778f
AnreyZlobin <zlobin.andy@gmail.com>
2022-03-13 17:21:05 +0300

    feat: update createDonateSchema

--- apps/backend/src/app/api/donate/validations/createDonateSchema.ts
+++ apps/backend/src/app/api/donate/validations/createDonateSchema.ts
@@ -4,7 +4,12 @@ import { CurrencyCodeEnum } from "../../currencies/types";
 
 export const createDonateSchema = yup.object({
   body: yup.object({
-    amount: yup.number().positive().required().min(1).typeError("Amount must be number"),
+    amount: yup
+      .number()
+      .positive()
+      .required()
+      .min(1, "Amount must be greater than or equal to 1")
+      .typeError("Amount must be number"),
     currency: yup.mixed().oneOf(Object.keys(CurrencyCodeEnum)).required(),
   }),
 });
